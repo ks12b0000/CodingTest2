e361a1277977237a24e91f58e9e4487e3f1b3bc6
ks12b0000 <ks12b0000@gmail.com>
2023-04-07 22:19:38 +0900

    2023-04-07

---
+++ CodingTest/src/Programmers/과제진행하기.java
@@ -0,0 +1,33 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class 과제진행하기 {
+//	public String[] solution(String[][] plans) {
+//        ArrayList<String> arr = new ArrayList<String>();
+//        Stack<String[]> stack = new Stack<String[]>();
+//        
+//        // 1. 시간순 정렬
+//        Arrays.sort(plans, (e1, e2) -> {
+//        	return time(e1[1]) - time(e2[1]);
+//        });
+//        
+//        
+//        // 2. 과제 진행
+//        // 3. 남아있는 시간 저장
+//        // 4. 끝나면 answer에 저장
+//        
+//        String[] answer = arr.toArray(new String[arr.size()]);
+//        return answer;
+//    }
+//	static int time(String str) {
+//		String[] time = str.split(":");
+//		int h = Integer.parseInt(time[0]) * 60;
+//		int m = Integer.parseInt(time[1]);
+//		
+//		return h + m;
+//	}
+}
+// 다시풀기
--- CodingTest/src/Programmers/연속된부분수열의합.java
+++ CodingTest/src/Programmers/연속된부분수열의합.java
@@ -2,39 +2,25 @@ package Programmers;
 
 
 public class 연속된부분수열의합 {
-//	public int[] solution(int[] sequence, int k) {
-//        int[] answer = new int[2];
-//        int[] arr = new int[sequence.length];
-//        arr[0] = sequence[0];
-//        
-//        for (int i = 1; i < sequence.length; i++) {
-//        	arr[i] = arr[i - 1] + sequence[i];
-//        	if (sequence[i] == k) {
-//        		answer[0] = i;
-//        		answer[1] = i;
-//        		break;
-//        	}
-//        	if (arr[i] == k) {
-//        		answer[0] = 0;
-//        		answer[1] = i;
-//        	}
-//        }
-//        
-//        for (int i = 0; i < arr.length - 1; i++) {
-//        	if (arr[i + 1] > k && arr[i] < k) {
-//        		answer[0] = i;
-//        		answer[1] = i + 1;
-//        	}
-//        }
-//        
-//        return answer;
-//    }
+	public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[2];
+        int left = 0, right = 0;
+        int sum = 0, min = Integer.MAX_VALUE;
+
+        while (right < sequence.length) {
+            sum += sequence[right++];
+
+            while (sum > k) {
+                sum -= sequence[left++];
+            }
+
+            if (sum == k && right - left < min) {
+            	min = right - left;
+                answer[0] = left;
+                answer[1] = right - 1;
+            }
+        }
+
+        return answer;
+    }
 }
-//1 2 3 4 5 
-//1 3 6 10 15
-
-//1 1 1 2 3 4 5
-//1 2 3 5 8 12 17
-
-//2 2 2 2 2
-//2 4 6 8 10
