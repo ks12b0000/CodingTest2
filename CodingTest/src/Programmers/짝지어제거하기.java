1a9f348fbea656e1c826a59f049b0e09a17e204f
ks12b0000 <ks12b0000@gmail.com>
2023-04-10 22:46:48 +0900

    2023-04-10

--- CodingTest/src/Programmers/짝지어제거하기.java
+++ CodingTest/src/Programmers/짝지어제거하기.java
@@ -1,5 +1,29 @@
 package Programmers;
 
-public class 짝지어제거하기 {
+import java.util.Stack;
 
+public class 짝지어제거하기 {
+	public int solution(String s) {
+        int answer = 0;
+        Stack<Character> stack = new Stack<Character>();
+        stack.push(s.charAt(0));
+        
+        for (int i = 1; i < s.length(); i++) {
+        	if (stack.isEmpty()) {
+        		stack.push(s.charAt(i));
+        	}
+        	else if (stack.peek() == s.charAt(i)) {
+        		stack.pop();
+        	}
+        	else {
+        		stack.push(s.charAt(i));
+        	}
+        }
+   
+        if (stack.isEmpty()) {
+        	answer = 1;
+        }
+        
+        return answer;
+    }
 }
--- CodingTest/src/Programmers/피보나치수.java
+++ CodingTest/src/Programmers/피보나치수.java
@@ -1,5 +1,20 @@
 package Programmers;
 
 public class 피보나치수 {
-
+	public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n + 1];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+        
+        for (int i = 3; i < arr.length; i++) {
+        	arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
+        }
+        
+        answer = arr[n];
+        
+        return answer;
+    }
 }
