f5d76871be60ac9e3c8471e42aa717bb0b5dc400
ks12b0000 <ks12b0000@gmail.com>
2023-04-27 21:04:10 +0900

    2023-04-27

---
+++ CodingTest/src/Programmers/이상한문자만들기.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+public class 이상한문자만들기 {
+	public String solution(String s) {
+        String answer = "";
+        String[] arr = s.split("");
+        int idx = 0;
+        
+        for (int i = 0; i < arr.length; i++) {
+    		if (arr[i].equals(" ")) {
+    			idx = 0;
+    		}
+    		else if (idx % 2 == 0) {
+        		arr[i] = arr[i].toUpperCase();
+        		idx++;
+        	}
+    		else if (idx % 2 != 0){
+    			arr[i] = arr[i].toLowerCase();
+    			idx++;
+    		}
+    		answer += arr[i];
+		}  
+        return answer;
+    }
+}
