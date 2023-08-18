a4b0019d22f0d9a3c8aac7ab55f52f5e4b2a1469
ks12b0000 <ks12b0000@gmail.com>
2023-08-18 18:11:07 +0900

    2023-08-18

--- CodingTest/src/Programmers/개인정보수집유효기간.java
+++ CodingTest/src/Programmers/개인정보수집유효기간.java
@@ -4,40 +4,39 @@ import java.util.ArrayList;
 
 public class 개인정보수집유효기간 {
 	public int[] solution(String today, String[] terms, String[] privacies) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
+        ArrayList<Integer> answer = new ArrayList<>();
         
-        int todayYY = Integer.parseInt(today.substring(0, 4));
-        int todayMM = Integer.parseInt(today.substring(5, 7));
-        int todayDD = Integer.parseInt(today.substring(8, 10));
+        int todayYear = Integer.parseInt(today.substring(0, 4));
+        int todayMonth = Integer.parseInt(today.substring(5, 7));
+        int todayDay = Integer.parseInt(today.substring(8, 10));
         
         for (int i = 0; i < privacies.length; i++) {
-        	int yy = Integer.parseInt(privacies[i].substring(0, 4));
-            int mm = Integer.parseInt(privacies[i].substring(5, 7));
-            int dd = Integer.parseInt(privacies[i].substring(8, 10));
+            int year = Integer.parseInt(privacies[i].substring(0, 4));
+            int month = Integer.parseInt(privacies[i].substring(5, 7));
+            int day = Integer.parseInt(privacies[i].substring(8, 10));
+            String type = privacies[i].substring(11, 12);
             
-            String tm = privacies[i].substring(11);
-            
-            for (int j = 0; j < terms.length; j++) {
-            	if (tm.equals(terms[j].charAt(0) + "")) {
-            		mm += Integer.parseInt(terms[j].substring(2));
-            		if (mm > 12) {
-            			yy += mm / 12;
-            			mm = mm % 12;
-            			if (mm == 0) {
-            				yy -= 1;
-            				mm = 12;
-            			}
-            		}
-            	}
+            for (String str : terms) {
+                if (type.equals(String.valueOf(str.charAt(0)))) {
+                    month += Integer.parseInt(str.substring(2, str.length()));
+                    if (month > 12) {
+                        year += month / 12;
+            			month %= 12;
+            			if (month == 0) {
+                            year--;
+                            month = 12;
+                        }
+                    }
+                }
             }
-            if (todayYY > yy) {
-            	answer.add(i + 1);
+            if (todayYear > year) {
+                answer.add(i + 1);
             }
-            else if (todayYY == yy && todayMM > mm) {
-            	answer.add(i + 1);
+            else if (todayYear == year && todayMonth > month) {
+                answer.add(i + 1);
             }
-            else if (todayYY == yy && todayMM == mm && todayDD >= dd) {
-            	answer.add(i + 1);
+            else if (todayYear == year && todayMonth == month && todayDay >= day) {
+                answer.add(i + 1);
             }
         }
         
