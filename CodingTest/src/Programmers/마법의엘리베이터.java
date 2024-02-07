821f1fe75dab760b9ef79873d4425427e7357406
ks12b0000 <ks12b0000@gmail.com>
2024-02-07 18:39:51 +0900

    2024-02-07

---
+++ CodingTest/src/Programmers/마법의엘리베이터.java
@@ -0,0 +1,27 @@
+package Programmers;
+
+public class 마법의엘리베이터 {
+	public int solution(int storey) {
+        int answer = 0;
+        
+        while (storey != 0) {
+            int n = storey % 10;
+            
+            storey /= 10;
+            
+            if (n < 5) {
+                answer += n;               
+            } else if (n > 5) {
+                answer += (10 - n);
+                storey++;               
+            } else if (storey % 10 >= 5) {
+                answer += 5;
+                storey++;
+            } else {
+                answer += 5;
+            }         
+        }
+        
+        return answer;
+    }
+}	
